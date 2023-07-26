ec827fde62c16a2d413bdc0b05584d89615ae14c
ahuigo <1781999+ahuigo@users.noreply.github.com>
2023-07-26 21:18:15 +0800

    fix(request): missing isdebugBody for request builder

--- examples/delete_test.go
+++ examples/delete_test.go
@@ -10,11 +10,14 @@ import (
 // Delete Form Request
 func TestDeleteForm(t *testing.T) {
 	println("Test DELETE method: form data(x-wwww-form-urlencoded)")
+	ts := createHttpbinServer(0)
+	defer ts.Close()
 	data := requests.Datas{
 		"comments": "ew",
 	}
+
 	session := requests.R() //.SetDebug()
-	resp, err := session.Delete("https://www.httpbin.org/delete", data)
+	resp, err := session.Delete(ts.URL+"/delete", data)
 	if err == nil {
 		fmt.Println(resp.Text())
 	}
